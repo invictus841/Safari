bc73553d315f12567189f43cb21e05ed09bdbbe6
NT <usedforfunny@gmail.com>
2022-04-06 09:17:24 +0200

    Initial Commit

---
+++ Safari/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Safari
+//
+//  Created by Alexandre Talatinian on 06/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Safari/SafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariApp.swift
+//  Safari
+//
+//  Created by Alexandre Talatinian on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
