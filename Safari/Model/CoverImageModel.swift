f3eac21a18c3905ffda1c0493f80f1547b780922
NT <usedforfunny@gmail.com>
2022-04-08 14:37:36 +0200

    Creation of an extension bundle to decode json files. CoverImages are now visible.

---
+++ Safari/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Safari
+//
+//  Created by Alexandre Talatinian on 07/04/2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable  {
+    let id: Int
+    let name: String
+}
