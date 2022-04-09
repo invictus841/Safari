b9281772e0a2be00af9fe15a4879222424d90ef3
NT <usedforfunny@gmail.com>
2022-04-09 18:44:44 +0200

    Creation of a data model "AnimalModel", to fetch data from json. Creation of a view "AnimalListItemView" . Implementing this view in ContentView, and using ForEach Loop to display all data.

---
+++ Safari/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalModel.swift
+//  Safari
+//
+//  Created by Alexandre Talatinian on 09/04/2022.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
