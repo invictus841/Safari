f7e4480260f17afc0d15107ef219b0d3377b8a94
NT <usedforfunny@gmail.com>
2022-04-13 09:59:41 +0200

    Creation of LocationModel. Creation of "MapAnnotationView". Implementing of MapAnnotationView in MapView. Custom advanced map annotations with nice effect.
    Overlay displaying longitude and latitude.

---
+++ Safari/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Safari
+//
+//  Created by Alexandre Talatinian on 12/04/2022.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D (latitude: latitude, longitude: longitude)
+    }
+}
